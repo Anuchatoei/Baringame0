932c98146482912dee2d5c02174ef4a5a2e0b641
Administrator <Anucha2801@gmail.com>
2017-01-07 09:02:56 +0700

    Initial commit

---
+++ app/src/main/java/rtc/anucha/kamonrat/baringame1/Animal10.java
@@ -0,0 +1,13 @@
+package rtc.anucha.kamonrat.baringame1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Animal10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal10);
+    }
+}
